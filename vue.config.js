9423b444d63f07821a8c213a9104cbd74e4af1b8
Majid Bahrehvar <mbahrehvar@invidi.com>
2025-05-14 14:09:18 -0600

    aallow

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false // This will prevent the linting errors during development
+  lintOnSave: false, // This will prevent the linting errors during development
+  devServer: {
+    allowedHosts: 'all' // For Webpack 5 compatibility, allows connections from any host
+  }
 })
