b9d9a159ffffc0773d5ad09fa5ad9d6d8660c544
Majid Bahrehvar <mbahrehvar@invidi.com>
2025-05-14 15:08:54 -0600

    set port

--- vue.config.js
+++ vue.config.js
@@ -4,8 +4,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false, // This will prevent the linting errors during development
   devServer: {
-    port: 8080, // Explicitly set port to 8080
+    port: 8085, // Explicitly set port to 8080
     allowedHosts: 'all', // For Webpack 5 compatibility, allows connections from any host
-    disableHostCheck: true
   }
 })
