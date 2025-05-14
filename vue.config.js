98afbe125a16b20c9bfab8535c7b51e11cdb10e3
Majid Bahrehvar <mbahrehvar@invidi.com>
2025-05-14 14:11:41 -0600

    aallow

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false, // This will prevent the linting errors during development
   devServer: {
-    allowedHosts: 'all' // For Webpack 5 compatibility, allows connections from any host
+    allowedHosts: 'all', // For Webpack 5 compatibility, allows connections from any host
+    disableHostCheck: true
   }
 })
